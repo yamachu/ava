b26ecc69c1cc990147322678fd041c09b5c010bc
yamachu <yamachu.dev@gmail.com>
2024-01-25 18:34:59 +0900

    add registerCompletionHandler to cjs

--- entrypoints/main.cjs
+++ entrypoints/main.cjs
@@ -1,2 +1,4 @@
 'use strict';
 module.exports = require('../lib/worker/main.cjs');
+module.exports.registerCompletionHandler
+	= require('../lib/worker/completion-handlers.js').registerCompletionHandler;
